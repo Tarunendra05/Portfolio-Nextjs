b1a32b75fd8bb43f010398be074722c4f458726f
Tarunendra Bahadur Singh <122162858+Tarunendra05@users.noreply.github.com>
2023-10-10 18:29:40 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Header from './components/Header';
+import Hero from "./components/Hero";
+import Services from "./components/Services";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+
+function App() {
+
+  const [darkMode, setDarkMode] = React.useState(false);
+
+  return (
+    <div className={darkMode && "dark"}>
+      <Header darkMode={darkMode} setDarkMode={setDarkMode}/>
+
+      <main className="bg-white dark:bg-gray-900 text-gray-900 dark:text-teal-300">
+        <Hero />
+        <Services />
+        <Projects />
+        <Contact />
+      </main>
+    </div>
+  );
+}
+
+export default App;
