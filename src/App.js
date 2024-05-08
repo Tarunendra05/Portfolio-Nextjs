e73e5df47336ca3079743559e9adb43c68b0a104
Tarunendra Bahadur Singh <122162858+Tarunendra05@users.noreply.github.com>
2024-05-08 17:49:10 +0530

    Update Locomotive JS

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import { LocomotiveScrollProvider } from "react-locomotive-scroll";
-import "./locomotive-scroll.css"
+import "./locomotive-scroll.css";
 
 function App() {
   const [darkMode, setDarkMode] = React.useState(true);
